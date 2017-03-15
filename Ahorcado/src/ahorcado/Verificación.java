8559677499d0446c1bfe7da2d36b5d99446fee24
sebastianver123 <sebastianver1234@gmail.com>
2017-03-15 17:24:57 -0500

    Add files via upload
    Juego con la palabra arbol

---
+++ Ahorcado/src/ahorcado/Verificación.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Verificación {
+    
+    char [] letras =new char[1];
+    
+    char simbolo = 'a';
+    
+  
+    
+    
+}
